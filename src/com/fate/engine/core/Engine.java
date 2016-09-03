d668c8d4b2da55856c1281e21ed49e70872deb50
Fernando Gonzalez <fallofrapture59@gmail.com>
2016-09-03 16:57:40 -0400

    Started engine framework using Game/GameApplication

---
+++ src/com/fate/engine/core/Engine.java
@@ -0,0 +1,45 @@
+package com.fate.engine.core;
+
+public class Engine extends Game {
+
+	public Engine(int width, int height, float fps, boolean fullscreen) {
+		super(width, height, "Game Engine", fps, fullscreen);
+	}
+
+	@Override
+	public void initGame() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void preGameUpdate() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void fixedGameUpdate(float dt) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void postGameUpdate() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void handleInput() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
