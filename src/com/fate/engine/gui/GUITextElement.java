1e9c7328e6f5f1bd8fee588e7b37806e7472609f
Fernando Gonzalez <fallofrapture59@gmail.com>
2016-09-04 21:17:16 -0400

    Changed EngineLauncher to main class in build.gradle

--- src/com/fate/engine/gui/GUITextElement.java
+++ src/com/fate/engine/gui/GUITextElement.java
@@ -2,6 +2,7 @@ package com.fate.engine.gui;
 
 import com.fate.engine.math.Vector2f;
 
+import org.lwjgl.stb.STBTruetype;
 
 public class GUITextElement extends GUIElement {
 	public GUITextElement(String text, String font, int size, Vector2f position) {
