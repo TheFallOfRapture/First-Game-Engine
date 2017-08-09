25876c8e7d0087395f7dd3144138af7a22c5d539
Fernando Gonzalez <fallofrapture59@gmail.com>
2017-08-09 21:40:08 +0000

    Request core profile 3.3

--- src/main/java/com/morph/engine/core/gui/EngineGUI.java
+++ src/main/java/com/morph/engine/core/gui/EngineGUI.java
@@ -36,23 +36,25 @@ public class EngineGUI extends GUI {
         float toolbarWidth = 250;
         float bottomBarHeight = 150;
 
-        addElement(new TextElement("File", "C:/Windows/Fonts/Calibri.ttf", 16, new Vector2f(5, height - menuBarHeight + 10)));
-        addElement(new TextElement("Edit", "C:/Windows/Fonts/Calibri.ttf", 16, new Vector2f(45, height - menuBarHeight + 10)));
-        addElement(new TextElement("Source", "C:/Windows/Fonts/Calibri.ttf", 16, new Vector2f(85, height - menuBarHeight + 10)));
-        addElement(new TextElement("Refactor", "C:/Windows/Fonts/Calibri.ttf", 16, new Vector2f(145, height - menuBarHeight + 10)));
-        addElement(new TextElement("Navigate", "C:/Windows/Fonts/Calibri.ttf", 16, new Vector2f(215, height - menuBarHeight + 10)));
-        addElement(new TextElement("Search", "C:/Windows/Fonts/Calibri.ttf", 16, new Vector2f(285, height - menuBarHeight + 10)));
-        addElement(new TextElement("Project", "C:/Windows/Fonts/Calibri.ttf", 16, new Vector2f(345, height - menuBarHeight + 10)));
-        addElement(new TextElement("Run", "C:/Windows/Fonts/Calibri.ttf", 16, new Vector2f(405, height - menuBarHeight + 10)));
-        addElement(new TextElement("Window", "C:/Windows/Fonts/Calibri.ttf", 16, new Vector2f(445, height - menuBarHeight + 10)));
-        addElement(new TextElement("Help", "C:/Windows/Fonts/Calibri.ttf", 16, new Vector2f(510, height - menuBarHeight + 10)));
+        String font = "C:/Windows/Fonts/Calibri.ttf";
+
+        addElement(new TextElement("File", font, 16, new Vector2f(5, height - menuBarHeight + 10)));
+        addElement(new TextElement("Edit", font, 16, new Vector2f(45, height - menuBarHeight + 10)));
+        addElement(new TextElement("Source", font, 16, new Vector2f(85, height - menuBarHeight + 10)));
+        addElement(new TextElement("Refactor", font, 16, new Vector2f(145, height - menuBarHeight + 10)));
+        addElement(new TextElement("Navigate", font, 16, new Vector2f(215, height - menuBarHeight + 10)));
+        addElement(new TextElement("Search", font, 16, new Vector2f(285, height - menuBarHeight + 10)));
+        addElement(new TextElement("Project", font, 16, new Vector2f(345, height - menuBarHeight + 10)));
+        addElement(new TextElement("Run", font, 16, new Vector2f(405, height - menuBarHeight + 10)));
+        addElement(new TextElement("Window", font, 16, new Vector2f(445, height - menuBarHeight + 10)));
+        addElement(new TextElement("Help", font, 16, new Vector2f(510, height - menuBarHeight + 10)));
         addElement(new Panel(new Vector2f(0, 0), new Vector2f(toolbarWidth, height - menuBarHeight), new Color(0x9E9E9E, 1), new Texture("textures/solid.png")));
         addElement(new Panel(new Vector2f(width - toolbarWidth, 0), new Vector2f(toolbarWidth, height - menuBarHeight), new Color(0x9E9E9E, 1), new Texture("textures/solid.png")));
         addElement(new Panel(new Vector2f(toolbarWidth, 0), new Vector2f(width - (toolbarWidth * 2), bottomBarHeight), new Color(0x757575, 1), new Texture("textures/solid.png")));
         addElement(new Panel(new Vector2f(0, height - menuBarHeight), new Vector2f(width, menuBarHeight), new Color(0x616161, 1), new Texture("textures/solid.png")));
 
-        testBtn1 = new Button("Test Button", "C:/Windows/Fonts/Calibri.ttf", 16, new Color(1, 1, 1), new Color(0.5f, 0.5f, 0.5f), new Texture("textures/friendlyBlueMan.png"), new Texture("textures/4Head.png"), new Transform2D(new Vector2f(500, 400), 0, new Vector2f(100, 50)), 0);
-        testBtn2 = new Button("Test Button 2", "C:/Windows/Fonts/Calibri.ttf", 16, new Color(1, 1, 1), new Color(0.5f, 0.5f, 0.5f), new Texture("textures/friendlyBlueMan.png"), new Texture("textures/4Head.png"), new Transform2D(new Vector2f(500, 600), 0, new Vector2f(100, 50)), 0);
+        testBtn1 = new Button("Test Button", font, 16, new Color(1, 1, 1), new Color(0.5f, 0.5f, 0.5f), new Texture("textures/friendlyBlueMan.png"), new Texture("textures/4Head.png"), new Transform2D(new Vector2f(500, 400), 0, new Vector2f(100, 50)), 0);
+        testBtn2 = new Button("Test Button 2", font, 16, new Color(1, 1, 1), new Color(0.5f, 0.5f, 0.5f), new Texture("textures/friendlyBlueMan.png"), new Texture("textures/4Head.png"), new Transform2D(new Vector2f(500, 600), 0, new Vector2f(100, 50)), 0);
 
         testBtn1.setOnHover(() -> System.out.println("Button 1 hovered over!"));
         testBtn2.setOnHover(() -> System.out.println("Button 2 hovered over!"));
--- src/main/java/com/morph/engine/graphics/GLDisplay.java
+++ src/main/java/com/morph/engine/graphics/GLDisplay.java
@@ -33,6 +33,11 @@ public class GLDisplay {
 		glfwDefaultWindowHints();
 		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
 		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
 		
 		window = GLFW.glfwCreateWindow(width, height, title, NULL, NULL);
 		if (window == NULL)
